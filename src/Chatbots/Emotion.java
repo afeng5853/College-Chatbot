e4db7b0979c968aa314af7072b5d0d423013e328
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-10-04 12:37:18 -0400

    added emotions

--- src/Chatbots/Emotion.java
+++ src/Chatbots/Emotion.java
@@ -1,10 +1,10 @@
 package Chatbots;
 
-public abstract class Emotion {
-	private int happy = 0;
-	private int sad = 0;
-	private int angry = 0;
-	private int humor = 0;
-	private int disgusted = 0;
+public interface Emotion {
+	int happy = 0;
+	int sad = 0;
+	int angry = 0;
+	int humor = 0;
+	int disgusted = 0;
 	
 }
