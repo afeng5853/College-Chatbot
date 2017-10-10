beac09b0c3ffed64519f22b41341d190f300a59c
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-10-10 12:30:37 -0400

    added the interface for multiple chatbots

--- src/ChatBotRunner.java
+++ src/ChatBotRunner.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 import chatbots.ChatBotBase;
+import chatbots.ChatBotChan;
 import chatbots.ChatBotCheung;
 import chatbots.ChatBotFeng;
 import chatbots.ChatBotZhou;
@@ -23,23 +24,79 @@ public class ChatBotRunner
 	 */
 	public static void main(String[] args) throws FileNotFoundException, IOException
 	{
-		//ChatBotCheung chatbot1 = new ChatBotCheung();
-		ChatBotFeng chatbot1 = new ChatBotFeng();
-		//ChatBotZhou chatbot2 = new ChatBotZhou();
-		//chatbot2.getResponse("My mom went to Princeton University, while my Dad went to Stony Brook");
+		ChatBotChan chatbot1 = new ChatBotChan();
+		ChatBotCheung chatbot2 = new ChatBotCheung();
+		ChatBotFeng chatbot3 = new ChatBotFeng();
+		ChatBotZhou chatbot4 = new ChatBotZhou();
 		
-		System.out.println (chatbot1.getGreeting());
+		
+		System.out.println("Hello! We are chatbots that will assist you with your college application!\n"
+							+ "Please pick a chatbot to talk to!\n"
+							+ "1. Chatbot Chan - Major Interests\n"
+							+ "2. Chatbot Cheung - Financial Aid\n"
+							+ "3. Chatbot Feng - General information about a college\n"
+							+ "4. Chatbot Zhou - Prepartion for the SAT");
 		Scanner in = new Scanner (System.in);
-		String statement = in.nextLine();
-		//System.out.println(chatbot1.getResponse(statement));
-
-		 
-		while (!statement.equals("Bye"))
-		{
-			System.out.println (chatbot1.getResponse(statement));
-			statement = in.nextLine();
+		
+		int choice;
+		while (true) {
+			try {
+				choice = Integer.parseInt(in.nextLine());
+				if (choice >= 1 && choice <= 4) {
+					break;
+				} else {
+					System.out.println("Please enter a valid number indicating the chatbot you want to talk to.");
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("Please enter a valid number indicating the chatbot you want to talk to.");
+			}
 		}
 		
+		// Greetings
+		switch (choice) {
+			case 1:
+			// Chan
+				System.out.println (chatbot1.getGreeting());
+				break;
+			case 2:
+			// Cheung
+				System.out.println (chatbot2.getGreeting());
+				break;
+			case 3:
+			// Feng
+				System.out.println (chatbot3.getGreeting());
+				break;
+			case 4:
+			// Zhou
+				System.out.println (chatbot4.getGreeting());
+				break;
+		// Responses
+		}
+		String statement = in.nextLine();
+		while (!statement.equals("Bye"))
+		{
+			switch (choice) {
+				case 1:
+				// Chan
+					System.out.println (chatbot1.getResponse(statement));
+					break;
+				case 2:
+				// Cheung
+					System.out.println (chatbot2.getResponse(statement));
+					break;
+				case 3:
+				// Feng
+					System.out.println (chatbot3.getResponse(statement));
+					break;
+				case 4:
+				// Zhou
+					System.out.println (chatbot4.getResponse(statement));
+					break;
+			}
+			
+			statement = in.nextLine();
+		}
+		in.close();
 	}
 
 }
