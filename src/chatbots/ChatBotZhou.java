df1a2ae860c7ff0b713b1dddbcb7c9b38e14622a
thomas <Prisoner@DESKTOP-14I6B8M>
2017-10-12 00:26:54 -0400

    Please ALEX

--- src/chatbots/ChatBotZhou.java
+++ src/chatbots/ChatBotZhou.java
@@ -2,115 +2,87 @@ package chatbots;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
-import java.util.Scanner;
 
 import colleges.CollegeParser;
 import grammar.GreetingsDictionary;
 import util.TextFileReader;
-
+import answers.answers;
 /**
- * A program to carry on conversations with a human user.
- * This version:
+ * SAT Practice Bot
+ * This specific chatbot will give the user SAT practice questions for the writing and math sections.
  * @author Thomas Zhou
- * @version September 2017
+ * @version 10.11
  */
 
-// u should add help keyword -alex
-public class ChatBotZhou extends ChatBotLevin
+/*
+ * Implement emotion
+ * greetingsDict.contains(statement.toLowerCase()) should work for punctuation
+ * After 5 questions finish, reset , score and sat estimate
+ * replace answer arrays with hashmaps possibly
+ * fix initialized variables
+ */
+public class ChatBotZhou extends ChatBotBase
 {
-	private static int STATE = 0;
-	private static final int SAT = 1;
-	private String satCategory = "";
-	private char answer;
-	private int testNum = 1;
+	private static String SECTION = "none";
+	private static boolean SAT = false; //Variable that tracks if testing is in progress
+	private static final String SECTION1 = "reading";
+	private static final String SECTION2 = "grammar";
+	private static final String SECTION3 = "math1";
+	private static final String SECTION4 = "math2";
+	int testNum = 1;
+	int score = 0;
+	
+	String[] reading = {"B","B","C","D","B"};
+	String[] grammar = {"D","B","A","C","B"}; 
+	String[] math1 = {"D","C","B","A","B"}; 
+	String[] math2 = {"D","B","D","B","107"};  
+	
 	public String getGreeting()
 	{
-		return "Hello! Would you like to take practice for reading, grammar, or math?";
+		return "Hello! I will help you in preparing for the SAT. Would you like to practice?";
 	}
-	//t
+	
 	public String getResponse(String statement) throws FileNotFoundException, IOException
 	{
-		String response = statement;
+		String response = genericResponse(statement);
 		GreetingsDictionary greetingsDict = new GreetingsDictionary();
-		if (STATE == SAT)
+		if (statement.length() == 0)
 		{
-			int score = 0;
-			int questionCount = 0;
-			Scanner input = new Scanner(System.in);
-			while (STATE == SAT)
-			{
-				String question = printQuestion(testNum, satCategory);
-				if (question.equals("")) {
-					STATE = 0;
-					testNum = 1;
-					double percentage = ((double) score / (double) questionCount) * 100;
-					System.out.println("You got " + percentage + "% correct");
-					if (percentage >= 90) {
-						System.out.println("Good job!");
-					} else if (percentage >= 70) {
-						System.out.println("Decent!");
-					} else {
-						System.out.println("You did really bad!");
-					}
-					System.out.println("Would you like to take a practice math, grammar, or reading exam?");
-					break;
-				}
-				System.out.println(question);
-				boolean out = false;
-				while (true) {
-					String userInput = input.nextLine();
-					if (findKeyword(userInput, "stop") != -1 || 
-							findKeyword(userInput, "pause") != -1 ||
-							findKeyword(userInput, "don't") != -1 ||
-							findKeyword(userInput, "do not") != -1 ||
-							findKeyword(userInput, "dont") != -1) {
-						System.out.println("Okay, would you like to take reading or math or grammar??");
-						STATE = 0;
-						testNum = 1;
-						out = true;
-						break;
-					}
-					if (userInput.toLowerCase().equals("a") ||
-							userInput.toLowerCase().equals("b") ||
-							userInput.toLowerCase().equals("c") ||
-							userInput.toLowerCase().equals("d")) {
-						if (userInput.toUpperCase().charAt(0) == answer) {
-							System.out.println("You got it correct!");
-							score++;
-						} else {
-							System.out.println("The correct answer was: " + answer);
-						}
-						questionCount++;
-						break;
-					} else {
-						System.out.println("Please type the corresponding later");
-					}
-				}
-				if (out) {
-					break;
-				}
-				testNum++;
-			}
-		}
-		else if (findKeyword(statement, "grammar") != -1) {
-			STATE = SAT;
-			satCategory = "grammar";
-			response = "Okay, any last words before you start?";
-		} 
-		else if (findKeyword(statement, "reading") != -1) {
-			STATE = SAT;
-			satCategory = "reading";
-			response = "Okay, any last words before you start?";
-		} 
-		else if (findKeyword(statement, "math") != -1) {
-			STATE = SAT;
-			satCategory = "math1";
-			response = "Okay, any last words before you start?";
+			response = "Please enter something.";
 		}
 		else if (greetingsDict.contains(statement.toLowerCase())) {
 			response = "Nice to meet you!";
-		} 
+		}
+		else if (SAT)
+		{
+			if (SECTION == SECTION1)
+			{
+				response = gradeQuestion(statement, reading);
+			}
+			if (SECTION == SECTION2)
+			{
+				response = gradeQuestion(statement, grammar);
+			}
+			else if (SECTION == SECTION3)
+			{
+				response = gradeQuestion(statement, math1);
+			}
+			if (SECTION == SECTION4)
+			{
+				response = gradeQuestion(statement, math2);
+			}
+		}
+		else if (findKeyword(statement, "prepare") != -1 || findKeyword(statement, "practice") != -1 || findKeyword(statement, "yes") != -1)
+		{
+			response = "Which section would you like to work on? For math, please specify if you would like to work on the calculator or no calculator. \n Reading \n Writing/Grammar \n Math";
+		}
+		else if (findKeyword(statement, "grammar") != -1 || findKeyword(statement, "math") != -1 || findKeyword(statement, "reading") != -1 
+				|| findKeyword(statement, "writing") != -1)
+		{
+			response = startSection(statement);
+		}
 		// If the user wants any of the other chatbots
 		else if (findKeyword(statement, "feng") != -1) {
 			System.out.println("Okay, bye!");
@@ -123,21 +95,47 @@ public class ChatBotZhou extends ChatBotLevin
 		else if (findKeyword(statement, "chan") != -1) {
 			System.out.println("Okay, bye!");
 			response = "~CALLCHAN";
-		} 
-		else if (findKeyword(statement, "chan") != -1) {
-			System.out.println("Okay, bye!");
-			response = "~CALLCHAN";
-		} 
-		else if (findKeyword(statement, "no") != -1) {
-			String[] bots = new String[] {"FENG", "CHEUNG", "CHAN"};
-			Random r = new Random();
-			int botChoice = r.nextInt(bots.length);
-			String botChoiceStr = bots[botChoice];
-			System.out.println("Okay then, go talk to Chatbot "  + botChoiceStr);
-			response = "~CALL" + botChoiceStr;
-		} 
-		else {
-			System.out.println("I don't know what you said");
+		}
+		return response;
+	}
+	
+	public String startSection(String statement) throws FileNotFoundException, IOException
+	{
+		String response = "";
+		if ((findKeyword(statement, "writing") != -1 || findKeyword(statement, "grammar") != -1) && findKeyword(statement, "reading") == -1 && findKeyword(statement, "math") == -1)
+		{
+			SECTION = SECTION2;
+			testNum = 1;
+			SAT = true;
+			response = printQuestion(testNum, SECTION);
+		}
+		if ((findKeyword(statement, "writing") == -1 && findKeyword(statement, "grammar") == -1) && findKeyword(statement, "reading") != -1 && findKeyword(statement, "math") == -1)
+		{
+			SECTION = SECTION1;
+			testNum = 1;
+			SAT = true;
+			response = printQuestion(testNum, SECTION);
+		}
+		if ((findKeyword(statement, "writing") == -1 && findKeyword(statement, "grammar") == -1) && findKeyword(statement, "reading") == -1 && findKeyword(statement, "math") != -1)
+		{
+			if (findKeyword(statement, "calculator") == -1)
+			{
+				response = "Which section would you like to work on? For math, please specify if you would like to work on the calculator or no calculator. \n Reading \n Writing/Grammar \n Math";
+			}
+			else if ((findKeyword(statement, "no") != -1))
+			{
+				SECTION = SECTION3;
+				testNum = 1;
+				SAT = true;
+				response = printQuestion(testNum, SECTION);
+			}
+			else
+			{
+				SECTION = SECTION4;
+				testNum = 1;
+				SAT = true;
+				response = printQuestion(testNum, SECTION);
+			}
 		}
 		return response;
 	}
@@ -150,7 +148,7 @@ public class ChatBotZhou extends ChatBotLevin
 		boolean isQuestion = false;
 		for (int i = 0; i < textfile.size(); i++)
 		{
-			String line = textfile.get(i).trim();
+			String line = textfile.get(i);
 			if (line.equals("") && isQuestion)
 			{
 				return response;
@@ -159,29 +157,38 @@ public class ChatBotZhou extends ChatBotLevin
 			{
 				response += line + "\n";
 			}
-			
-			//Second part of condition is still broken
-			if (line.length() > 0 && (!Character.isAlphabetic(line.charAt(0)) && Integer.parseInt(Character.toString(line.charAt(0))) == number))
+			if (line.length() > 0 && Character.isDigit(line.charAt(0)) && Integer.parseInt(Character.toString(line.charAt(0))) == number)
 			{
 				isQuestion = true;
-				answer = line.charAt(3);
 			}
 		}
-		return "";
+		return response;
 	}
-	 
-	public String printL(int num) throws FileNotFoundException, IOException
+	
+	public String gradeQuestion(String statement, String[] answer) throws FileNotFoundException, IOException
 	{
-		TextFileReader text = new TextFileReader("src/sat problems/grammar.txt");
-		ArrayList<String> textfile = text.getDict();
-		return textfile.get(6);
+		String response = "";
+		if (statement.toUpperCase().equals(answer[testNum-1]))
+		{
+			testNum++;
+			if (testNum > 5)
+			{
+				SAT = false;
+				return "You finished! Would you like to work on another section?";
+			}
+			response = "Correct! \n" + printQuestion(testNum, SECTION);
+			score++;
+		}
+		else
+		{
+			testNum++;
+			if (testNum > 5)
+			{
+				SAT = false;
+				return "You finished! Would you like to work on another section?";
+			}
+			response = "Incorrect! The correct answer is " + answer[testNum-1] + ". \n" + printQuestion(testNum, SECTION);
+		}
+		return response; 
 	}
 }
-/*
-String response = "";
-
-CollegeParser  = new CollegeParser(statement);
-ArrayList<String> colleges = test.getColleges();
-System.out.println(colleges);
-return response;
-*/
